074509a8f0620fa6febae0abcb7209b0255e3aa6
boris.katzman <borisk58@gmail.com>
2023-05-06 19:03:18 +0300

    Initial commit

---
+++ websocket-ms/src/main/java/com/borisk58/websocketms/WebsocketMsApplication.java
@@ -0,0 +1,13 @@
+package com.borisk58.websocketms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketMsApplication.class, args);
+	}
+
+}
---
+++ websocket-ms/src/test/java/com/borisk58/websocketms/WebsocketMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borisk58.websocketms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
